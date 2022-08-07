e04d9fc3a45c2f6ad687ae6eb233fd0326007427
Kateryna82 <ksvidnytska@gmail.com>
2022-08-07 22:10:06 +0200

    Add forecast

--- src/app.js
+++ src/app.js
@@ -32,6 +32,10 @@ function displayWeather(response) {
     document.querySelector("#humidity").innerHTML = Math.round(response.data.main.humidity);
     document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
     document.querySelector("#description").innerHTML = response.data.weather[0].main;
+    document.querySelector("#icon").setAttribute("src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  document.querySelector("#icon").setAttribute("alt", response.data.weather[0].description);
   }
   
   function newCity(event) {
@@ -41,6 +45,34 @@ function displayWeather(response) {
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayWeather);
   }
-  let currentCity = document.querySelector("#search-form");
-  currentCity.addEventListener("submit", newCity);
  
+ let currentCity = document.querySelector("#search-form");
+  currentCity.addEventListener("submit", newCity);
+
+  function displayForecast() {
+    let forecastElement = document.querySelector("#fore");
+    let days = ["Sun", "Mon", "Tue", "Wed"];
+    let forecastHTML = `<div class = "row">`;
+    days.forEach(function (day) {
+    forecastHTML = forecastHTML + `
+    <div class="col-2">
+      <div class="weather-forecast-date">${day}</div>
+      <img
+        src="http://openweathermap.org/img/wn/50d@2x.png"
+        alt=""
+        width="42"
+      />
+      <div class="weather-forecast-temperatures">
+        <span class="weather-forecast-temperature-max"> 18° </span>
+        <span class="weather-forecast-temperature-min"> 12° </span>
+      </div>
+    </div>
+`;
+    });
+
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+    
+  }
+  displayForecast();
+  
